989c3b2db4cdb8ca23430a9a7e08accb9e26d77a
eshaank <eshaank@gmail.com>
2024-08-11 00:14:51 -0700

    Initial Commit

---
+++ SystemMonitor/SystemMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemMonitorApp.swift
+//  SystemMonitor
+//
+//  Created by Eshaan Kansagara on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SystemMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SystemMonitorUITests/SystemMonitorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SystemMonitorUITestsLaunchTests.swift
+//  SystemMonitorUITests
+//
+//  Created by Eshaan Kansagara on 8/11/24.
+//
+
+import XCTest
+
+final class SystemMonitorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
